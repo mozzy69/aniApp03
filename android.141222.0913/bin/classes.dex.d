7b8a118ac595baeb69359e43f8037b7aba44d88d
lyndon <mozzy69@yahoo.com>
2014-12-22 10:14:37 +0200

    exporting signed App tests

---
+++ android.141222.0913/bin/classes.dex.d
@@ -0,0 +1,15 @@
+/home/lyndon/sketchbook/aniApp03/android/bin/classes.dex : \
+/home/lyndon/sketchbook/aniApp03/android/bin/classes/processing/test/aniapp03/aniApp03$PickColor.class \
+/home/lyndon/sketchbook/aniApp03/android/bin/classes/processing/test/aniapp03/aniApp03$Header.class \
+/home/lyndon/sketchbook/aniApp03/android/bin/classes/processing/test/aniapp03/R.class \
+/home/lyndon/sketchbook/aniApp03/android/bin/classes/processing/test/aniapp03/aniApp03$Bead.class \
+/home/lyndon/sketchbook/aniApp03/android/bin/classes/processing/test/aniapp03/aniApp03.class \
+/home/lyndon/sketchbook/aniApp03/android/bin/classes/processing/test/aniapp03/R$attr.class \
+/home/lyndon/sketchbook/aniApp03/android/bin/classes/processing/test/aniapp03/aniApp03$Controls.class \
+/home/lyndon/sketchbook/aniApp03/android/bin/classes/processing/test/aniapp03/R$layout.class \
+/home/lyndon/sketchbook/aniApp03/android/bin/classes/processing/test/aniapp03/R$drawable.class \
+/home/lyndon/sketchbook/aniApp03/android/bin/classes/processing/test/aniapp03/BuildConfig.class \
+/home/lyndon/sketchbook/aniApp03/android/bin/classes/processing/test/aniapp03/aniApp03$Frame.class \
+/home/lyndon/sketchbook/aniApp03/android/bin/dexedLibs/SelectFile-ed312e6e74c5005ec48d4b0079a50df6.jar \
+/home/lyndon/sketchbook/aniApp03/android/bin/dexedLibs/annotations-2b8b55af61c2ff34b709e2cdc035dfad.jar \
+/home/lyndon/sketchbook/aniApp03/android/bin/dexedLibs/processing-core-e2fcfd4df58581eebbae6bba695ee81d.jar \
---
+++ android.141222.0913/gen/R.java.d
@@ -0,0 +1,9 @@
+/home/lyndon/sketchbook/aniApp03/android/gen/processing/test/aniapp03/R.java \
+ : /home/lyndon/sketchbook/aniApp03/android/res/drawable-ldpi/icon.png \
+/home/lyndon/sketchbook/aniApp03/android/res/drawable/icon.png \
+/home/lyndon/sketchbook/aniApp03/android/res/drawable-hdpi/icon.png \
+/home/lyndon/sketchbook/aniApp03/android/res/layout/main.xml \
+/home/lyndon/sketchbook/aniApp03/android/bin/res/drawable-ldpi/icon.png \
+/home/lyndon/sketchbook/aniApp03/android/bin/res/drawable/icon.png \
+/home/lyndon/sketchbook/aniApp03/android/bin/res/drawable-hdpi/icon.png \
+/home/lyndon/sketchbook/aniApp03/android/bin/AndroidManifest.xml \
